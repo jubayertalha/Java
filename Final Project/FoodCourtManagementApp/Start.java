557bf8a0bb53ffb93749377d69d36128daf8395f
Talha Jubayer <jubayertalha194@gmail.com>
2020-05-15 09:38:35 +0600

    fix exceptions

--- Final Project/FoodCourtManagementApp/Start.java
+++ Final Project/FoodCourtManagementApp/Start.java
@@ -26,22 +26,93 @@ public class Start {
 
 	}
 
+	private static String getLineString(){
+		String s = "";
+		try {
+			s = bfr.readLine();
+		} catch (IOException e) {
+			s = "null";
+			sc = new Scanner(System.in);
+		} catch (InputMismatchException e){
+			s = "null";
+			sc = new Scanner(System.in);
+		} catch (NoSuchElementException e){
+			s = "null";
+			sc = new Scanner(System.in);
+		}
+		if(s==null){
+			s = "null";
+		}
+		return s;
+	}
+
+	private static String getString(){
+		String s = "";
+		try {
+			s = sc.next();
+		} catch (InputMismatchException e){
+			s = "null";
+			sc = new Scanner(System.in);
+		} catch (NoSuchElementException e){
+			s = "null";
+			sc = new Scanner(System.in);
+		}
+		return s;
+	}
+
+	private static double getDouble(){
+		double d = 0;
+		try {
+			d = sc.nextDouble();
+		} catch (InputMismatchException e){
+			d = -1;
+			sc = new Scanner(System.in);
+		} catch (NoSuchElementException e){
+			d = -1;
+			sc = new Scanner(System.in);
+		}
+		return d;
+	}
+
+	private static int getInt(){
+		int i = 0;
+		try {
+			i = sc.nextInt();
+		} catch (InputMismatchException e){
+			i = -1;
+			sc = new Scanner(System.in);
+		} catch (NoSuchElementException e){
+			i = -1;
+			sc = new Scanner(System.in);
+		}
+		return i;
+	}
+
 	private static void insertNewEmployee(FoodCourt foodCourt) {
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.println("'''''''''''    Insert New Employee    '''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		Employee employee = new Employee();
+		String name = "";
+		String id = "";
+		double salary = 0;
 		System.out.print("\tEnter Employee Name: ");
-		try {
-			employee.setName(bfr.readLine());
-		} catch (IOException e) {
-			//e.printStackTrace();
-		}
+		name = getLineString();
 		System.out.print("\tEnter Employee Id: ");
-		employee.setEmpId(sc.next());
+		id = getString();
 		System.out.print("\tEnter Employee Salary: ");
-		employee.setSalary(sc.nextDouble());
-		foodCourt.insertEmployee(employee);
+		salary = getDouble();
+		if(!name.equals("null")&&!id.equals("null")&&salary!=-1){
+			employee.setSalary(salary);
+			employee.setEmpId(id);
+			employee.setName(name);
+			foodCourt.insertEmployee(employee);
+		}else{
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			System.out.println("\t"+"Sorry.. The input is invalid. Please try again!");
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			System.out.println();
+		}
 	}
 
 	private static void removeExistingEmployee(FoodCourt foodCourt) {
@@ -49,7 +120,7 @@ public class Start {
 		System.out.println("'''''''    Remove Existing Employee    ''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Employee Id: ");
-		foodCourt.removeEmployee(foodCourt.getEmployee(sc.next()));
+		foodCourt.removeEmployee(foodCourt.getEmployee(getString()));
 	}
 
 	private static void showAllEmployees(FoodCourt foodCourt) {
@@ -64,7 +135,7 @@ public class Start {
 		System.out.println("''''''''''    Search An Employee    '''''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Employee Id: ");
-		Employee e = foodCourt.getEmployee(sc.next());
+		Employee e = foodCourt.getEmployee(getString());
 		if (e != null) {
 			System.out.println("#################################################");
 			System.out.println("\tEmployee Name: " + e.getName());
@@ -85,15 +156,22 @@ public class Start {
 		System.out.println("''''''''''     Insert New Restaurant    '''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		Restaurant restaurant = new Restaurant();
+		String name = "";
+		String id = "";
 		System.out.print("\tEnter Restaurant Name: ");
-		try {
-			restaurant.setName(bfr.readLine());
-		} catch (IOException e) {
-			//e.printStackTrace();
-		}
+		name = getLineString();
 		System.out.print("\tEnter Restaurent Id: ");
-		restaurant.setRid(sc.next());
-		foodCourt.insertRestaurant(restaurant);
+		id = getString();
+		if(!name.equals("null")&&!id.equals("null")){
+			restaurant.setName(name);
+			restaurant.setRid(id);
+			foodCourt.insertRestaurant(restaurant);;
+		}else{
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			System.out.println("\t"+"Sorry.. The input is invalid. Please try again!");
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			System.out.println();
+		}
 	}
 
 	private static void removeExistingRestaurant(FoodCourt foodCourt) {
@@ -101,7 +179,7 @@ public class Start {
 		System.out.println("''''''     Remove Existing Restaurant    ''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurant Id: ");
-		foodCourt.removeRestaurant(foodCourt.getRestaurant(sc.next()));
+		foodCourt.removeRestaurant(foodCourt.getRestaurant(getString()));
 	}
 
 	private static void showAllRestaurants(FoodCourt foodCourt) {
@@ -116,7 +194,7 @@ public class Start {
 		System.out.println("''''''''''     Search A Restaurant    '''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurent Id: ");
-		Restaurant r = foodCourt.getRestaurant(sc.next());
+		Restaurant r = foodCourt.getRestaurant(getString());
 		if (r != null) {
 			System.out.println("#################################################");
 			System.out.println("\tRestaurant Name: " + r.getName());
@@ -138,7 +216,7 @@ public class Start {
 		System.out.println("''''''''''      Insert New FoodITem     '''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurant Id: ");
-		Restaurant restaurant = foodCourt.getRestaurant(sc.next());
+		Restaurant restaurant = foodCourt.getRestaurant(getString());
 		int option = 0;
 		if (restaurant == null) {
 			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -149,51 +227,73 @@ public class Start {
 			System.out.println("\t\t1. Insert New MainDish.");
 			System.out.println("\t\t2. Insert New Appitizer.");
 			System.out.print("\t\tEnter Your Option: ");
-			option = sc.nextInt();
-		}
-		if (option == 1 && restaurant != null) {
-			MainDish mainDish = new MainDish();
-			System.out.print("\t\tEnter MainDish Name: ");
-			try {
-				mainDish.setName(bfr.readLine());
-			} catch (IOException e) {
-				// e.printStackTrace();
+			option = getInt();
+			if (option == 1) {
+				MainDish mainDish = new MainDish();
+				String name = "";
+				String category = "";
+				double price = 0;
+				int quantity = 0;
+				String id = "";
+				System.out.print("\t\tEnter MainDish Name: ");
+				name = getLineString();
+				System.out.print("\t\tEnter MainDish Category: ");
+				category = getLineString();
+				System.out.print("\t\tEnter MainDish Price: ");
+				price = getDouble();
+				System.out.print("\t\tEnter MainDish Available Quantity: ");
+				quantity = getInt();
+				System.out.print("\t\tEnter MainDish Id: ");
+				id = getString();
+				if(!name.equals("null")&&!category.equals("null")&&price!=-1&&quantity!=-1&&!id.equals("null")){
+					mainDish.setName(name);
+					mainDish.setCategory(category);
+					mainDish.setPrice(price);
+					mainDish.setAvailableQuantity(quantity);
+					mainDish.setFid(id);
+					restaurant.insertFoodItem(mainDish);
+				}else{
+					System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					System.out.println("\t"+"Sorry.. The input is invalid. Please try again!");
+					System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					System.out.println();
+				}
+			} else if (option == 2) {
+				Appetiziers appetiziers = new Appetiziers();
+				String name = "";
+				String size = "";
+				double price = 0;
+				int quantity = 0;
+				String id = "";
+				System.out.print("\t\tEnter Appitizier Name: ");
+				size = getLineString();
+				System.out.print("\t\tEnter Appitizer Size: ");
+				size = getString();
+				System.out.print("\t\tEnter Appitizer Price: ");
+				price = getDouble();
+				System.out.print("\t\tEnter Appitizer Available Quantity: ");
+				quantity = getInt();
+				System.out.print("\t\tEnter Appitizer Id: ");
+				id = getString();
+				if(!name.equals("null")&&!size.equals("null")&&price!=-1&&quantity!=-1&&!id.equals("null")){
+					appetiziers.setName(name);
+					appetiziers.setSize(size);
+					appetiziers.setPrice(price);
+					appetiziers.setAvailableQuantity(quantity);
+					appetiziers.setFid(id);
+					restaurant.insertFoodItem(appetiziers);
+				}else{
+					System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					System.out.println("\t"+"Sorry.. The input is invalid. Please try again!");
+					System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					System.out.println();
+				}
+			}else{
+				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				System.out.println("\t"+"Sorry.. The input is invalid. Please try again!");
+				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				System.out.println();
 			}
-			System.out.print("\t\tEnter MainDish Category: ");
-			try {
-				mainDish.setCategory(bfr.readLine());
-			} catch (IOException e) {
-				// e.printStackTrace();
-			}
-			System.out.print("\t\tEnter MainDish Price: ");
-			mainDish.setPrice(sc.nextDouble());
-			System.out.print("\t\tEnter MainDish Available Quantity: ");
-			mainDish.setAvailableQuantity(sc.nextInt());
-			System.out.print("\t\tEnter MainDish Id: ");
-			mainDish.setFid(sc.next());
-			restaurant.insertFoodItem(mainDish);
-		} else if (option == 2 && restaurant != null) {
-			Appetiziers appetiziers = new Appetiziers();
-			System.out.print("\t\tEnter Appitizier Name: ");
-			try {
-				appetiziers.setName(bfr.readLine());
-			} catch (IOException e) {
-				//e.printStackTrace();
-			}
-			System.out.print("\t\tEnter Appitizer Size: ");
-			appetiziers.setSize(sc.next());
-			System.out.print("\t\tEnter Appitizer Price: ");
-			appetiziers.setPrice(sc.nextDouble());
-			System.out.print("\t\tEnter Appitizer Available Quantity: ");
-			appetiziers.setAvailableQuantity(sc.nextInt());
-			System.out.print("\t\tEnter Appitizer Id: ");
-			appetiziers.setFid(sc.next());
-			restaurant.insertFoodItem(appetiziers);
-		}else{
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			System.out.println("\t"+"Sorry.. The input is invalid. Please try again!");
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			System.out.println();
 		}
 	}
 
@@ -202,7 +302,7 @@ public class Start {
 		System.out.println("'''''''     Remove Existing FoodItem    '''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurant Id: ");
-		Restaurant restaurant = foodCourt.getRestaurant(sc.next());
+		Restaurant restaurant = foodCourt.getRestaurant(getString());
 		if(restaurant == null){
 			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			System.out.println("\t"+"Sorry.. There is no matching restaurant with this Id. Please try again!");
@@ -210,7 +310,7 @@ public class Start {
 			System.out.println();
 		}else{
 			System.out.print("\tEnter FoodItem Id: ");
-			restaurant.removeFoodItem(restaurant.getFoodItem(sc.next()));
+			restaurant.removeFoodItem(restaurant.getFoodItem(getString()));
 		}
 	}
 
@@ -219,7 +319,7 @@ public class Start {
 		System.out.println("''''''''''      Show All FoodItems     ''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurant Id: ");
-		Restaurant restaurant = foodCourt.getRestaurant(sc.next());
+		Restaurant restaurant = foodCourt.getRestaurant(getString());
 		if(restaurant == null){
 			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			System.out.println("\t"+"Sorry.. There is no matching restaurant with this Id. Please try again!");
@@ -235,7 +335,7 @@ public class Start {
 		System.out.println("''''''''''     Search A FoodItem    '''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurent Id: ");
-		Restaurant r = foodCourt.getRestaurant(sc.next());
+		Restaurant r = foodCourt.getRestaurant(getString());
 		if(r == null){
 			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			System.out.println("\t"+"Sorry.. There is no matching restaurant with this Id. Please try again!");
@@ -243,7 +343,7 @@ public class Start {
 			System.out.println();
 		}else{
 			System.out.print("\tEnter FoodItem Id: ");
-			FoodItem f = r.getFoodItem(sc.next());
+			FoodItem f = r.getFoodItem(getString());
 			if(f != null){
 				f.showInfo();
 			}else{
@@ -260,7 +360,7 @@ public class Start {
 		System.out.println("'''''''''''''      Add FoodItem    ''''''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurent Id: ");
-		Restaurant r = foodCourt.getRestaurant(sc.next());
+		Restaurant r = foodCourt.getRestaurant(getString());
 		if(r == null){
 			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			System.out.println("\t"+"Sorry.. There is no matching restaurant with this Id. Please try again!");
@@ -268,25 +368,26 @@ public class Start {
 			System.out.println();
 		}else{
 			System.out.print("\tEnter FoodItem Id: ");
-			FoodItem f = r.getFoodItem(sc.next());
+			FoodItem f = r.getFoodItem(getString());
 			if(f != null){
 				System.out.print("\tEnter Amount You Want to Add: ");
-				int amount = sc.nextInt();
-
-				String s = "*************************************************\r\n";
-				s += "\tActivity Type: Add"+"\r\n";
-				s += "\tFood Id: "+f.getFid()+"\r\n";
-				s += "\tFood Name: "+f.getName()+"\r\n";
-				s += "\tAdding Quantity: "+amount+"\r\n";
-				s += "\tPrevious Stock: "+f.getAvailableQuantity()+"\r\n";
-				s += "\tCurrent Stock: "+(f.getAvailableQuantity()+amount)+"\r\n";
-				s += "\tAdding Restaurant Id: "+r.getRid()+"\r\n";
-				s += "\tAdding Restaurant Name: "+r.getName()+"\r\n";
-				s += "*************************************************\r\n";
-
-				fileOperation.write(s+"\r"+"\n");
-
+				int amount = getInt();
+				int previous = f.getAvailableQuantity();
+				int current = previous+amount;
 				f.addQuantity(amount);
+				if(amount>=1){
+					String s = "*************************************************\r\n";
+					s += "\tActivity Type: Add"+"\r\n";
+					s += "\tFood Id: "+f.getFid()+"\r\n";
+					s += "\tFood Name: "+f.getName()+"\r\n";
+					s += "\tAdding Quantity: "+amount+"\r\n";
+					s += "\tPrevious Stock: "+previous+"\r\n";
+					s += "\tCurrent Stock: "+current+"\r\n";
+					s += "\tAdding Restaurant Id: "+r.getRid()+"\r\n";
+					s += "\tAdding Restaurant Name: "+r.getName()+"\r\n";
+					s += "*************************************************\r\n";
+					fileOperation.write(s+"\r"+"\n");
+				}
 			}else{
 				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 				System.out.println("\t"+"Sorry.. There is no matching food item with this Id. Please try again!");
@@ -301,7 +402,7 @@ public class Start {
 		System.out.println("'''''''''''''     Sell FoodItem    ''''''''''''''");
 		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''");
 		System.out.print("\tEnter Restaurent Id: ");
-		Restaurant r = foodCourt.getRestaurant(sc.next());
+		Restaurant r = foodCourt.getRestaurant(getString());
 		if(r == null){
 			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			System.out.println("\t"+"Sorry.. There is no matching restaurant with this Id. Please try again!");
@@ -309,25 +410,26 @@ public class Start {
 			System.out.println();
 		}else{
 			System.out.print("\tEnter FoodItem Id: ");
-			FoodItem f = r.getFoodItem(sc.next());
+			FoodItem f = r.getFoodItem(getString());
 			if(f != null){
 				System.out.print("\tEnter Amount You Want to Sell: ");
-				int amount = sc.nextInt();
-
-				String s = "*************************************************\r\n";
-				s += "\tActivity Type: Sell"+"\r\n";
-				s += "\tFood Id: "+f.getFid()+"\r\n";
-				s += "\tFood Name: "+f.getName()+"\r\n";
-				s += "\tSelling Quantity: "+amount+"\r\n";
-				s += "\tPrevious Stock: "+f.getAvailableQuantity()+"\r\n";
-				s += "\tCurrent Stock: "+(f.getAvailableQuantity()-amount)+"\r\n";
-				s += "\tSeller Restaurant Id: "+r.getRid()+"\r\n";
-				s += "\tSeller Restaurant Name: "+r.getName()+"\r\n";
-				s += "*************************************************\r\n";
-
-				fileOperation.write(s+"\r"+"\n");
-
+				int amount = getInt();
+				int previous = f.getAvailableQuantity();
+				int current = previous-amount;
 				f.sellQuantity(amount);
+				if(amount<1&&amount>previous){
+					String s = "*************************************************\r\n";
+					s += "\tActivity Type: Sell"+"\r\n";
+					s += "\tFood Id: "+f.getFid()+"\r\n";
+					s += "\tFood Name: "+f.getName()+"\r\n";
+					s += "\tSelling Quantity: "+amount+"\r\n";
+					s += "\tPrevious Stock: "+previous+"\r\n";
+					s += "\tCurrent Stock: "+current+"\r\n";
+					s += "\tSeller Restaurant Id: "+r.getRid()+"\r\n";
+					s += "\tSeller Restaurant Name: "+r.getName()+"\r\n";
+					s += "*************************************************\r\n";
+					fileOperation.write(s+"\r"+"\n");
+				}
 			}else{
 				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 				System.out.println("\t"+"Sorry.. There is no matching food item with this Id. Please try again!");
@@ -357,7 +459,7 @@ public class Start {
 			System.out.println("\t4. FoodItem Quantity Add-Sell");
 			System.out.println("\t5. Exit");
 			System.out.print("\tEnter Your Option: ");
-			int option = sc.nextInt();
+			int option = getInt();
 			switch(option){
 				case 1:
 					showEmployeeManagementMenu(foodCourt);
@@ -399,7 +501,7 @@ public class Start {
 			System.out.println("\t4. Search an Employee");
 			System.out.println("\t5. Go Back");
 			System.out.print("\tEnter Your Option: ");
-			int option = sc.nextInt();
+			int option = getInt();
 			switch(option){
 				case 1:
 					insertNewEmployee(foodCourt);
@@ -438,7 +540,7 @@ public class Start {
 			System.out.println("\t4. Search a Restaurent");
 			System.out.println("\t5. Go Back");
 			System.out.print("\tEnter Your Option: ");
-			int option = sc.nextInt();
+			int option = getInt();
 			switch(option){
 				case 1:
 					insertNewRestaurant(foodCourt);
@@ -477,7 +579,7 @@ public class Start {
 			System.out.println("\t4. Search a FoodItem");
 			System.out.println("\t5. Go Back");
 			System.out.print("\tEnter Your Option: ");
-			int option = sc.nextInt();
+			int option = getInt();
 			switch(option){
 				case 1:
 					insertNewFoodItem(foodCourt);
@@ -515,7 +617,7 @@ public class Start {
 			System.out.println("\t3. Show Add Sell History");
 			System.out.println("\t4. Go Back");
 			System.out.print("\tEnter Your Option: ");
-			int option = sc.nextInt();
+			int option = getInt();
 			switch(option){
 				case 1:
 					addFoodItem(foodCourt);
